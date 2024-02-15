42fb5c30918ebb2f0e87e3f7a9a4c80b422c2d6b
joaobug <joaoenfrance@gmail.com>
2024-02-15 10:57:26 -0300

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Joao Leal on 2/15/24.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
